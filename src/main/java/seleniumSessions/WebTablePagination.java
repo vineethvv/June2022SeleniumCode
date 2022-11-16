268698136f620850717bae9005d4f9775e3c3484
Vineeth Ganesh <vineeth.ganesh@teranet.ca>
2022-11-16 12:27:10 +0530

    pagination code updated

--- src/main/java/seleniumSessions/WebTablePagination.java
+++ src/main/java/seleniumSessions/WebTablePagination.java
@@ -1,5 +1,7 @@
 package seleniumSessions;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,42 +19,70 @@ public class WebTablePagination {
 		
 		Thread.sleep(3000);
 		
+		//Multiple selection
+        while(true) {
+			
+			if(driver.findElements(By.xpath("//td[text()='India']")).size()>0) {
+				
+				selectMultipleCountry("India");
 		
-		//Single selection
-		while(true) {
+			}
 			
-			if(driver.findElements(By.xpath("//td[text()='Denmark']")).size()>0) {
-				selectCountry("Denmark");
+			WebElement next = driver.findElement(By.linkText("Next"));
+			
+			if(next.getAttribute("class").contains("disabled")) {
+				
+				System.out.println("Pagination is over ...");
 				break;
-				
-			}
-			else {
-				// Pagination logic
-				
-				System.out.println("Country not found....Clicking on next");
-				WebElement next = driver.findElement(By.linkText("Next"));
-				
-				if(next.getAttribute("class").contains("disabled")) {
-					
-					System.out.println("Pagination is over ...Country still not found...");
-					break;
-				}
-				
-				next.click();
-				
-				
 			}
 			
+			next.click();
+			Thread.sleep(1000);
 			
-		}
+        }
+		
+		/*
+		 * //Single selection while(true) {
+		 * 
+		 * if(driver.findElements(By.xpath("//td[text()='Denmark']")).size()>0) {
+		 * selectCountry("Denmark"); break;
+		 * 
+		 * } else { // Pagination logic
+		 * 
+		 * System.out.println("Country not found....Clicking on next"); WebElement next
+		 * = driver.findElement(By.linkText("Next"));
+		 * 
+		 * if(next.getAttribute("class").contains("disabled")) {
+		 * 
+		 * System.out.println("Pagination is over ...Country still not found...");
+		 * break; }
+		 * 
+		 * next.click();
+		 * 
+		 * 
+		 * }
+		 * 
+		 * 
+		 * }
+		 */
 
 	}
-	
-	
+
+	public static void selectMultipleCountry(String countryName) {
+
+		List<WebElement> countryList = driver
+				.findElements(By.xpath("//td[text()='India']/preceding-sibling::td/input[@type='checkbox']"));
+
+		for (WebElement e : countryList) {
+
+			e.click();
+		}
+	}
+
 	public static void selectCountry(String CountryName) {
-		
-		
-		driver.findElement(By.xpath("//td[text()='"+CountryName+"']/preceding-sibling::td/input[@type='checkbox']")).click();
+
+		driver.findElement(By.xpath("//td[text()='" + CountryName + "']/preceding-sibling::td/input[@type='checkbox']"))
+				.click();
 	}
 
 }
